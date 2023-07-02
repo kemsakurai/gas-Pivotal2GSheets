ed8e254b7f19f31b3381e65903b209f0e5f4b2b1
kemsakurai <sakurai.kem@mail.com>
2023-07-02 13:12:31 +0900

    Update main.ts
    Program execution completed.
    Pre-deployment state, deploy and test.

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,44 @@ interface ReleasesAndStories {
 function onOpen() {
   const lang = Session.getActiveUserLocale();
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu("gas-GmailAutoPurge")
+  ui.createMenu("gas-pivotal2GSheets")
       .addItem(lang === "ja" ? "設定" : "Settings", "setValues")
-      .addToUi();
-}
-function createRealeaseList() {
+      .addItem(lang === "ja" ? "変更履歴を作成する" : "Create Changelog", "createChangeLogs")
+      .addToUi();}
+
+function createChangeLogs() {
   const projectId= Number(PropertiesService.getScriptProperties().getProperty("PROJECT_ID")) ?? 0;
   const releases = Utils.fetchReleases(projectId);
-
   const releasesAndStories: ReleasesAndStories = {};
   for(const release of releases) {
     releasesAndStories[release.id] = Utils.fetchStories(projectId, release.id);
   }
+  const rows = [];
+  const header = [];
+  header.push("Release Name");
+  header.push("Release Status");
+  header.push("Release Date");
+  header.push("Ticket Name");
+  header.push("Story Point");
+  header.push("Story Type");
+  header.push("Story URL");
+  rows.push(header);
+  for(const release of releases) {
+    const stories = releasesAndStories[release.id];
+    for (const story of stories) {
+      const columns = [];
+      columns.push(release.name);
+      columns.push(release.current_state);
+      columns.push(release.deadline);
+      columns.push(story.name);
+      columns.push(story.estimate);
+      columns.push(story.story_type);
+      columns.push(story.url);
+      rows.push(columns);
+    }
+  }
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("ChangeLogs");
+  sheet?.getRange(1,1, rows.length, 7).setValues(rows);
 
 }
 function setValues () {
@@ -45,4 +71,5 @@ function setValues () {
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare let global: any;
 global.onOpen = onOpen;
-global.createRealeaseList = createRealeaseList;
+global.createChangeLogs = createChangeLogs;
+global.setValues = setValues;
